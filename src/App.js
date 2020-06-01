39b15ddfa2aef5e91a35e097f698f672ecde38ef
bbdoggett3 <bbdoggett3@gmail.com>
2020-06-01 09:30:26 -0600

    morning review

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import './routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
 
     </div>
   );
